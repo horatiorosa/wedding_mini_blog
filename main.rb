17772827b303f6e593c7fc8d0376776bf3fb0045
antonio brown <aeb10462@gmail.com>
2015-04-30 17:33:17 -0400

    created signin and signup forms

--- main.rb
+++ main.rb
@@ -11,15 +11,15 @@ get '/' do
 	erb	:home
 end
 
-post '/sign-in' do
-	@user = User.where(username: params[:username]).first
-		if @user.password == params[:password]
-			redirect '/'
-		else
-			redirect '/login-failed'
-		end
-end
+# post '/sign-in' do
+# 	@user = User.where(username: params[:username]).first
+# 		if @user.password == params[:password]
+# 			redirect '/'
+# 		else
+# 			redirect '/login-failed'
+# 		end
+# end
 
-get '/login-failed' do
-	"Incorrect log-in. Please retry."
-end
+# get '/login-failed' do
+# 	"Incorrect log-in. Please retry."
+# end
