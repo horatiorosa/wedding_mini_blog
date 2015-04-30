751d5043befe4ce76074567de4a4fb7b4d33e6cd
Mai Tamakoshi <mai23coco@gmail.com>
2015-04-30 17:56:20 -0400

    edited welcome.erb

--- main.rb
+++ main.rb
@@ -4,38 +4,12 @@ require 'bundler/setup'
 require 'rack-flash'
 require './models'
 
+set :database, "sqlite3:exercise.sqlite3"
+set :sessions, true
+use Rack::Flash, sweep: true
 
-set :database, "sqllite3.wed_db.sqllite3"
 
-get '/' do
-	@user = User.all
-	erb	:home
+get '/' do 
+	erb :welcome
+
 end
-
-# post '/sign-in' do
-# 	@user = User.where(username: params[:username]).first
-# 		if @user.password == params[:password]
-# 			redirect '/'
-# 		else
-# 			redirect '/login-failed'
-# 		end
-# end
-
-# get '/login-failed' do
-# 	"Incorrect log-in. Please retry."
-# end
-
-
-get '/signup' do
-	erb	:signup
-end
-
-
-post '/signup' do
-	user = User.new(params[:attribute, :attribute, :attribute])
-	if user.save
-		redirect '/home'
-	else
-		redirect '/signup' #or maybe an error try again alert
-	end
-
