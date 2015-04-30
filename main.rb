3b0ad0f95218023d3835979ac76982b34a4f9540
Horatio J Rosa <horatiorosa@aol.com>
2015-04-30 18:47:09 -0400

    pulling to push

--- main.rb
+++ main.rb
@@ -19,26 +19,27 @@ end
 
 
 post '/signup' do
-	user = User.new(params[:attribute, :attribute, :attribute])
+	@user = User.new(params[:user])
 	if user.save
 		redirect '/home'
 	else
 		redirect '/signup' #or maybe an error try again alert
 	end
-
-
-
-
-
-
-post '/sign-in' do
-	@user = User.where(username: params[:username]).first
-		if @user.password == params[:password]
-			redirect '/'
-		else
-			redirect '/login-failed'
-		end
 end
+
+
+
+
+
+
+# post '/sign-in' do
+# 	@user = User.where(username: params[:username]).first
+# 		if @user.password == params[:password]
+# 			redirect '/'
+# 		else
+# 			redirect '/login-failed'
+# 		end
+# end
 
 # get '/login-failed' do
 # 	"Incorrect log-in. Please retry."
