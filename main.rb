f01a1b01a412628a2b7626f84df11da6405ac88a
Horatio J Rosa <horatiorosa@aol.com>
2015-04-30 14:47:32 -0400

    updated main.rb and layout.erb with barebones info

--- main.rb
+++ main.rb
@@ -3,3 +3,23 @@ require 'sinatra/activerecord'
 require 'bundler/setup'
 require 'rack-flash'
 require './models'
+
+
+set :database, "sqllite3.wed_db.sqllite3"
+
+get '/' do
+	erb	:home
+end
+
+post '/sign-in' do
+	@user = User.where(username: params[:username]).first
+		if @user.password == params[:password]
+			redirect '/'
+		else
+			redirect '/login-failed'
+		end
+end
+
+get '/login-failed' do
+	"Incorrect log-in. Please retry."
+end
