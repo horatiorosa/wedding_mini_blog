cdc99a4a03868685633875ec564720cb078cae50
Mai Tamakoshi <mai23coco@gmail.com>
2015-04-30 17:51:37 -0400

    created welcome.erb

--- main.rb
+++ main.rb
@@ -3,3 +3,13 @@ require 'sinatra/activerecord'
 require 'bundler/setup'
 require 'rack-flash'
 require './models'
+
+set :database, "sqlite3:exercise.sqlite3"
+set :sessions, true
+use Rack::Flash, sweep: true
+
+
+get '/' do 
+	erb :welcome
+
+end
